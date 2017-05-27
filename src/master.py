eba2776607e8bc9a1c7fd3b7fe52a2fe8c4459d3
Murtaza Fatakdawala <murtazaaz98@gmail.com>
2017-05-27 09:20:06 -0400

    Modified src file to save both uneditted and editted files and other minor changes

--- src/master.py
+++ src/master.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cv2
 import os
+import copy
 
 def remove_background(imagePath):
-	cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-	 	#print("Load the Image")
+	#cv2.namedWindow('image', cv2.WINDOW_NORMAL)
+	#print("Load the Image")
 	imgo = cv2.imread(imagePath)
 	height, width = imgo.shape[:2]
 	 
@@ -15,7 +16,7 @@ def remove_background(imagePath):
 	bgdModel = np.zeros((1,65),np.float64)
 	fgdModel = np.zeros((1,65),np.float64)
 	 
-	#print("Hard Coding the Rect The object must lie within this rect.")
+	#print("Hard Coding the Rectangle, The object must lie within this rect.")
 	rect = (10,10,width-30,height-30)
 	cv2.grabCut(imgo,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
 
@@ -32,21 +33,25 @@ def remove_background(imagePath):
 	#print("Add the background and the image")
 	final = background + img1
 
-	#print("To be done - Smoothening the edges")
-	cv2.imshow('image', final)	 
-	# k = cv2.waitKey(0)	 
-	# if k==27:
-	#     cv2.destroyAllWindows()
+	
+	# cv2.imshow('image', final) # debug statement	 
+	newPath = ('.').join(imagePath.split('.')[:-1])
+	cv2.imwrite(newPath + '-editted' + '.jpg',final)
+	# k = cv2.waitKey(0)	# debug statement 
+	# if k==27: # debug statement
+	#     cv2.destroyAllWindows() # debug statement
 
 
 def images_iterator(imgsPath,finalPath):
 	imgs = os.listdir(imgsPath)
 	for i in range (0, len(imgs)):
 		remove_background(imgsPath + '/' + imgs[i])
-		os.rename(os.path.abspath(imgsPath + imgs[i]), finalPath + imgs[i])
+		newimgName = ('.').join(imgs[i].split('.')[:-1])
+		print (imgsPath + newimgName+ '-editted' + '.jpg') # debug statement
+		os.rename(imgsPath + newimgName+ '-editted' + '.jpg', finalPath + imgs[i])#+ '-editted' + '.jpg')
 
 def main():
-	path = '/media/sf_ubuntu-sf/EngHack 2017/'
+	path = '/media/sf_ubuntu-sf/outfitted/other/'
 	oldtopsDir = path + 'oldTops/' 
 	oldbotsDir = path + 'oldBottoms/'
 	topsDir = path + 'Tops/'
